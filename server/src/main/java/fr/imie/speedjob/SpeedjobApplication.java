6909b9ec18eef4f823012cf7418a2bfada9ce478
Kilyan Fossey <fosseykilyan@gmail.com>
2018-02-21 12:10:04 +0100

    Initial server commit

---
+++ server/src/main/java/fr/imie/speedjob/SpeedjobApplication.java
@@ -0,0 +1,11 @@
+package fr.imie.speedjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedjobApplication {
+		public static void main(String[] args) {
+		SpringApplication.run(SpeedjobApplication.class, args);
+	}
+}
