51fd76dd2c7f2c91cca1066b7854a16b2f5e0808
Kilyan Fossey <fosseykilyan@gmail.com>
2018-03-19 17:19:45 +0100

    Pre-merge

--- client/src/components/business/signUpBusiness.vue
+++ client/src/components/business/signUpBusiness.vue
@@ -1,150 +1,166 @@
 <template>
-  <v-container xs12 sm10>
-    <v-card class="elevation-2 scroll-30">
-      <v-parallax src="https://wallpapersite.com/images/wallpapers/material-design-1920x1200-geometric-stock-dark-black-hd-10125.jpg" height="300">
-        <v-card-title primary-title>
-          <div class="mx-auto">
-            <div class="display-3 intro">Speed Job</div>
-            <div class="mt-2">Plateforme d'embauche mettant en relation les entreprises et l'école du numérique IMIE</div>
-          </div>
-        </v-card-title>
-      </v-parallax>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex md10 offset-md1>
+        <v-card class="elevation-2 mb-5">
+          <v-parallax src="https://wallpapersite.com/images/wallpapers/material-design-1920x1200-geometric-stock-dark-black-hd-10125.jpg" height="300">
+            <v-card-title primary-title>
+              <div class="mx-auto">
+                <div class="display-3 intro-inline ma-4">SpeedJob</div>
+                <div class="title intro ma-2">Plateforme d'embauche mettant en relation les entreprises et l'école du numérique IMIE</div>
+              </div>
+            </v-card-title>
+          </v-parallax>
 
-      <v-card class="elevation-12">
-        <v-card-title>
-            <div class="headline mx-auto">INFORMATIONS</div>
-        </v-card-title>
-        <v-card-text>
-          <v-form v-model="valid" ref="form" lazy-validation>
-            <v-layout row wrap align-baseline>
-              <v-flex xs12 sm4 offset-sm1>
-                <v-text-field
-                  name="lastName"
-                  label="Nom"
-                  :rules="lastNameRules"
-                  v-model="lastName"
-                ></v-text-field>
-                <v-text-field
-                  name="firstName"
-                  label="Prénom"
-                  :rules="firstNameRules"
-                  v-model="firstName"
-                ></v-text-field>
-                <v-text-field
-                  name="mail"
-                  label="Mail"
-                  :rules="mailRules"
-                  v-model="mail"
-                ></v-text-field>
-                <v-text-field
-                  name="phone"
-                  label="Numéro de téléphone"
-                  :rules="phoneRules"
-                  v-model="phone"
-                ></v-text-field>
-                <v-text-field
-                  name="job"
-                  label="Poste / fonction dans l'entreprise"
-                  :rules="jobRules"
-                  v-model="job"
-                ></v-text-field>
-                <v-text-field
-                  name="password"
-                  label="Mot de passe"
-                  hint="Au moins 6 caractères"
-                  :rules="passwordRules"
-                  v-model="password"
-                  min="6"
-                  :append-icon="passwordHide ? 'visibility' : 'visibility_off'"
-                  :append-icon-cb="() => (passwordHide = !passwordHide)"
-                  :type="passwordHide ? 'password' : 'text'"
-                  counter
-                ></v-text-field>
-                <v-text-field
-                  name="password"
-                  label="Confirmez votre mot de passe"
-                  :rules="passwordRepetitionRules"
-                  v-model="passwordRepetition"
-                  min="6"
-                  :type="'password'"
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm4 offset-sm2>
-                <v-text-field
-                  name="businessName"
-                  label="Raison sociale"
-                ></v-text-field>
-                <v-text-field
-                  name="streetName"
-                  label="Adresse : rue, impasse..."
-                ></v-text-field>
-                <v-layout row wrap>
-                  <v-flex xs6>
+          <v-card class="elevation-12">
+            <v-card-title>
+              <div class="headline mx-auto">INFORMATIONS</div>
+            </v-card-title>
+            <v-card-text>
+              <v-form v-model="valid" ref="form" lazy-validation>
+                <v-layout row wrap align-baseline>
+                  <v-flex xs12 sm4 offset-sm1>
                     <v-text-field
-                      name="postalCode"
-                      label="Code Postal"
+                      name="lastName"
+                      label="Nom"
+                      :rules="lastNameRules"
+                      v-model="lastName"
+                    ></v-text-field>
+                    <v-text-field
+                      name="firstName"
+                      label="Prénom"
+                      :rules="firstNameRules"
+                      v-model="firstName"
+                    ></v-text-field>
+                    <v-text-field
+                      name="mail"
+                      label="Mail"
+                      :rules="mailRules"
+                      v-model="mail"
+                    ></v-text-field>
+                    <v-text-field
+                      name="phone"
+                      label="Numéro de téléphone"
+                      :rules="phoneRules"
+                      v-model="phone"
+                    ></v-text-field>
+                    <v-text-field
+                      name="job"
+                      label="Poste / fonction dans l'entreprise"
+                      :rules="jobRules"
+                      v-model="job"
+                    ></v-text-field>
+                    <v-text-field
+                      name="password"
+                      label="Mot de passe"
+                      hint="Au moins 6 caractères"
+                      :rules="passwordRules"
+                      v-model="password"
+                      min="6"
+                      :append-icon="passwordHide ? 'visibility' : 'visibility_off'"
+                      :append-icon-cb="() => (passwordHide = !passwordHide)"
+                      :type="passwordHide ? 'password' : 'text'"
+                      counter
+                    ></v-text-field>
+                    <v-text-field
+                      name="password"
+                      label="Confirmez votre mot de passe"
+                      :rules="passwordRepetitionRules"
+                      v-model="passwordRepetition"
+                      min="6"
+                      :type="'password'"
                     ></v-text-field>
                   </v-flex>
-                  <v-flex xs6>
+                  <v-flex xs12 sm4 offset-sm2>
+                    <v-select
+                      label="Raison sociale"
+                      autocomplete
+                      :loading="loadingBusiness"
+                      cache-items
+                      :items="businesses"
+                      :rules="[() => businessName.length > 0 || 'La raison sociale est obligatoire']"
+                      :search-input.sync="searchBusiness"
+                      v-model="businessName"
+                    ></v-select>
                     <v-text-field
-                      name="city"
-                      label="Ville"
+                      name="streetName"
+                      label="Adresse : rue, impasse..."
+                      :rules="streetNameRules"
+                      v-model="streetName"
                     ></v-text-field>
+                    <v-layout row wrap>
+                      <v-flex xs6>
+                        <v-text-field
+                          name="postalCode"
+                          label="Code Postal"
+                          :rules="postalCodeRules"
+                          v-model="postalCode"
+                        ></v-text-field>
+                      </v-flex>
+                      <v-flex xs6>
+                        <v-text-field
+                          name="city"
+                          label="Ville"
+                          :rules="cityRules"
+                          v-model="city"
+                        ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                    <v-text-field
+                      name="businessPhone"
+                      label="Téléphone de la société"
+                      :rules="phoneRules"
+                      v-model="businessPhone"
+                    ></v-text-field>
+                    <v-text-field
+                      name="businessMail"
+                      label="Mail de la société"
+                      :rules="mailRules"
+                      v-model="businessMail"
+                    ></v-text-field>
+                    <v-layout row wrap>
+                      <v-flex xs6 offset-xs3>
+                        <div class="input-picture">
+                          <picture-input
+                            ref="pictureInput"
+                            @change="onChangeImage"
+                            accept="image/jpeg,image/png,image/gif,application/pdf"
+                            width="200"
+                            height="200"
+                            size="2"
+                            radius="2"
+                            margin="10"
+                            removable
+                            :zIndex="1"
+                            :customStrings="{
+                              drag: 'Déposez un logo',
+                              remove: 'Supprimer le logo'
+                            }">
+                          </picture-input>
+                        </div>
+                      </v-flex>
+                    </v-layout>
                   </v-flex>
                 </v-layout>
-                <v-text-field
-                  name="businessPhone"
-                  label="Téléphone de la société"
-                  :rules="phoneRules"
-                  v-model="businessPhone"
-                ></v-text-field>
-                <v-text-field
-                  name="businessMail"
-                  label="Mail de la société"
-                  :rules="mailRules"
-                  v-model="businessMail"
-                ></v-text-field>
-                <v-layout row wrap>
-                  <v-flex xs6 offset-xs3>
-                    <div class="input-picture">
-                      <picture-input
-                        ref="pictureInput"
-                        @change="onChangeImage"
-                        accept="image/jpeg,image/png,image/gif,application/pdf"
-                        width="200"
-                        height="200"
-                        size="2"
-                        radius="2"
-                        margin="10"
-                        removable
-                        :zIndex="1"
-                        :customStrings="{
-                        drag: 'Déposez un logo',
-                        remove: 'Supprimer le logo'
-                      }">
-                      </picture-input>
-                    </div>
-                  </v-flex>
-                </v-layout>
-              </v-flex>
-            </v-layout>
-            <v-btn
-              @click="submit"
-              fab
-              large
-              absolute
-              right
-              color="success"
-              class="mt-3 mb-3 btn-validate"
-            >
-              <v-icon>
-                send
-              </v-icon>
-            </v-btn>
-          </v-form>
-        </v-card-text>
-      </v-card>
-    </v-card>
+                <v-btn
+                  @click="submit"
+                  fab
+                  large
+                  absolute
+                  right
+                  color="success"
+                  class="mt-3 mb-3 btn-validate"
+                >
+                  <v-icon>
+                    send
+                  </v-icon>
+                </v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -157,16 +173,17 @@
     },
     data: function () {
       return {
+        loadingBusiness: false,
         valid: false,
         firstName: '',
         firstNameRules: [
           v => !!v || 'Le prénom est obligatoire',
-          v => (v && v.length <= 21) || 'Le prénom doit être inférieur ou égal à 20 caractères'
+          v => (v && v.length <= 20) || 'Le prénom doit être inférieur ou égal à 20 caractères'
         ],
         lastName: '',
         lastNameRules: [
           v => !!v || 'Le nom de famille est obligatoire',
-          v => (v && v.length <= 41) || 'Le nom de famille doit être inférieur ou égal à 40 caractères'
+          v => (v && v.length <= 40) || 'Le nom de famille doit être inférieur ou égal à 40 caractères'
         ],
         mail: '',
         mailRules: [
@@ -181,14 +198,14 @@
         job: '',
         jobRules: [
           v => !!v || 'Le poste est obligatoire',
-          v => (v && v.length > 20) || 'Le titre du poste doit être inférieur ou égal à 20 caractères'
+          v => (v && v.length <= 20) || 'Le titre du poste doit être inférieur ou égal à 20 caractères'
         ],
         passwordHide: true,
         password: '',
         passwordRules: [
           v => !!v || 'Le mot de passe est obligatoire',
           v => (v && v.length >= 6) || 'Le mot de passe doit contenir au moins 6 caractères',
-          v => (v && v.length <= 31) || 'Le mot de passe doit contenir moins de 30 caractères'
+          v => (v && v.length <= 30) || 'Le mot de passe doit contenir moins de 30 caractères'
         ],
         passwordRepetition: '',
         passwordRepetitionRules: [
@@ -196,13 +213,35 @@
           v => (v && v === this.password) || 'Les mots de passe ne correspondent pas'
         ],
         businessName: '',
+        businessNameRules: [
+          v => !!v || 'La raison sociale est obligatoire',
+          v => (v && v.length <= 30) || 'La raison sociale doit contenir moins de 30 caractères'
+        ],
         streetName: '',
+        streetNameRules: [
+          v => !!v || 'L\'adresse est obligatoire',
+        ],
         streetNumber: '',
+        streetNumberRules: [
+          v => !!v || 'Le numéro de rue est obligatoire',
+        ],
         postalCode: '',
+        postalCodeRules: [
+          v => !!v || 'Le code postal est obligatoire',
+          v => /^([0-9]){5}$/.test(v) || 'Le code postal est invalide',
+        ],
         city: '',
+        cityRules: [
+          v => !!v || 'La ville est obligatoire'
+        ],
         businessPhone: '',
         businessMail: '',
         image: null
+      }
+    },
+    watch: {
+      searchBusiness (val) {
+        val && val.length > 1 && this.searchBusinessAsync(val)
       }
     },
     methods: {
@@ -215,6 +254,10 @@
         } else {
           console.log('FileReader API not supported: use the <form>, Luke!')
         }
+      },
+      searchBusinessAsync (v) {
+        this.loadingBusiness = true
+        this.$http.get('/back/businesses').then(console.log('ok'), console.log('ko'))
       },
       submit: function (event) {
         var inputs = document.getElementsByClassName('oblg')
@@ -269,10 +312,14 @@
     background-color #48b9c7
     padding 5px
 
+  @media screen and (min-width: 576px)
+    .mx-sm-20px
+      margin 200px
+
   #submit
     margin-left auto
 
-  .intro
+  .intro-inline
     font-family 'Intro Inline'
 
   .btn-validate
@@ -282,7 +329,4 @@
 
   .input-picture
     margin-bottom 50px
-
-  .scroll-30
-    margin-bottom 30px
 </style>
