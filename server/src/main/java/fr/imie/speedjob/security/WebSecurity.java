de4970d8c38dea650e82a25fa293f4aa0fa95de5
Eric Mouster <eric.mouster@gmail.com>
2018-04-16 14:58:04 +0200

    Resolve CORS Origin

--- server/src/main/java/fr/imie/speedjob/security/WebSecurity.java
+++ server/src/main/java/fr/imie/speedjob/security/WebSecurity.java
@@ -1,8 +1,9 @@
 package fr.imie.speedjob.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,8 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static fr.imie.speedjob.security.SecurityConstants.SIGN_UP_URL;
@@ -33,8 +34,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
             .anyRequest().authenticated()
             .and()
-            .cors()
-            .and()
+	    .cors()
+	    .and()
             .addFilter(new JWTAuthenticationFilter(authenticationManager()))
             .addFilter(new JWTAuthorizationFilter(authenticationManager()))
             // this disables session creation on Spring Security
@@ -46,7 +47,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
   }
 
-
   @Bean
   public CorsFilter corsFilter() {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -57,5 +57,5 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     config.addAllowedMethod("*");
     source.registerCorsConfiguration("/**", config);
     return new CorsFilter(source);
-  }
+ }
 }
