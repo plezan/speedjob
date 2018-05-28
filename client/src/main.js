008c119f6e4cfeb69967d66088bd401778859367
Kilyan Fossey <fosseykilyan@gmail.com>
2018-05-29 00:31:34 +0200

    LiveReload implementation

--- client/src/main.js
+++ client/src/main.js
@@ -4,10 +4,8 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
-import VueResource from 'vue-resource'
 
 Vue.use(Vuetify)
-Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
